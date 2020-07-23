3f48026ef0ffbdd0f1441cb5e62c74f5c179710f
azrinurvani <azrinurvani@gmail.com>
2020-07-23 14:43:01 +0700

    Live Data Reactive Streams using MVVM

--- app/src/main/java/com/mobile/azrinurvani/dagger2kotlinpractice/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/mobile/azrinurvani/dagger2kotlinpractice/ui/auth/AuthActivity.kt
@@ -2,13 +2,19 @@ package com.mobile.azrinurvani.dagger2kotlinpractice.ui.auth
 
 import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.bumptech.glide.RequestManager
 import com.mobile.azrinurvani.dagger2kotlinpractice.R
+import com.mobile.azrinurvani.dagger2kotlinpractice.models.User
 import com.mobile.azrinurvani.dagger2kotlinpractice.viewmodels.ViewModelProviderFactory
 import dagger.android.support.DaggerAppCompatActivity
 import kotlinx.android.synthetic.main.activity_auth.*
 import javax.inject.Inject
+
 
 class AuthActivity : DaggerAppCompatActivity() {
 
@@ -32,13 +38,36 @@ class AuthActivity : DaggerAppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
 
-        //TODO 9 - initialize viewModel atribut using providerFactory
+        //TODO 9 - initialize viewModel atribute using providerFactory
         viewModel = ViewModelProviders.of(this,providerFactory).get(AuthViewModel::class.java)
 
+        //TODO 24 - Add this for login_button action and call subscribeObserver
+        login_button.setOnClickListener {
+            attemptLogin()
+        }
+
+        subscribeObserver()
         setLogo()
     }
 
     private fun setLogo(){
         requestManager.load(logo).into(login_logo)
+    }
+
+    //TODO 22 - create function to attempt login
+    private fun attemptLogin(){
+        if (TextUtils.isEmpty(user_id_input.text.toString())){
+            return
+        }
+        viewModel.authenticateWithId(user_id_input.text.toString().toInt())
+    }
+
+    //TODO 23 - Create subscribeOrbsever, untuk memproses source yang ada pada authenticateWithId function
+    private fun subscribeObserver(){
+        viewModel.observeUser().observe(this, Observer<User?> { user ->
+            if (user != null) {
+                Log.d(TAG, "onChanged: " + user.email)
+            }
+        })
     }
 }
--- app/src/main/java/com/mobile/azrinurvani/dagger2kotlinpractice/ui/auth/AuthViewModel.kt
+++ app/src/main/java/com/mobile/azrinurvani/dagger2kotlinpractice/ui/auth/AuthViewModel.kt
@@ -1,14 +1,23 @@
 package com.mobile.azrinurvani.dagger2kotlinpractice.ui.auth
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.LiveDataReactiveStreams
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.ViewModel
+import com.mobile.azrinurvani.dagger2kotlinpractice.models.User
 import com.mobile.azrinurvani.dagger2kotlinpractice.network.AuthApi
 import io.reactivex.Observer
-import io.reactivex.Scheduler
+import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
-class AuthViewModel @Inject constructor(authApi: AuthApi) : ViewModel() {
+
+class AuthViewModel @Inject constructor(private val authApi: AuthApi) : ViewModel() {
+
+//    TODO 19 - Create private mediator live data variabel
+    private val authUser = MediatorLiveData<User>()
+
     companion object{
         private const val TAG = "AuthViewModel"
     }
@@ -17,14 +26,50 @@ class AuthViewModel @Inject constructor(authApi: AuthApi) : ViewModel() {
         Log.d(TAG,"AuthViewModel is working...")
 
         //TODO 17 - Call getUser method from AuthApi
-        authApi.getUser(1)
-            .toObservable()
-            .subscribeOn(Schedulers.io())
-            .subscribe ({user->
-                Log.d(TAG,"onNext : "+user.email)
-            },{
-                Log.e(TAG,"onError : "+it.localizedMessage)
-            })
+        //TODO 18 - Delete this block program base TODO 17
+//        authApi.getUser(1)
+//            .toObservable()
+//            .subscribeOn(Schedulers.io())
+//            .subscribe ({user->
+//                Log.d(TAG,"onNext : "+user.email)
+//            },{
+//                Log.e(TAG,"onError : "+it.localizedMessage)
+//            })
 
+    }
+    //TODO 21 - create auhthtenticateWithId function, merubah source berupa live data ke dalam bentuk observer
+    fun authenticateWithId(userId :Int) {
+        val source : LiveData<User> = LiveDataReactiveStreams.fromPublisher(
+            authApi.getUser(userId).
+                    subscribeOn(Schedulers.io())
+        )
+        authUser.addSource(source, object : Observer<User?>, androidx.lifecycle.Observer<User> {
+            override fun onChanged(user: User?) {
+                authUser.value = user
+                authUser.removeSource(source)
+            }
+
+            override fun onComplete() {
+
+            }
+
+            override fun onSubscribe(d: Disposable) {
+
+            }
+
+            override fun onNext(t: User) {
+
+            }
+
+            override fun onError(e: Throwable) {
+
+            }
+        })
+    }
+
+
+    //TODO 20 - Create observeUser function to return authUser
+    fun observeUser() : LiveData<User>{
+        return authUser
     }
 }
